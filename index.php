586f71e9d52888301673cb93ecaf8bcbe90f892c
MrPhoenixWright <baburinmihail223@gmail.com>
2023-01-23 16:59:08 +0700

    three commit

--- index.php
+++ index.php
@@ -59,24 +59,197 @@
          'job' => 'babysitter',
      ],
  ];
- //разбиваю изначальный масив на части и извлекаю surname,name,patronomyc
- function person (){
-     global $example_persons_array;
+ //сама функция склеивания
+ function getFullnameFromParts($stroka1,$stroka2,$stroka3){
+     $my_fullname = $stroka1." ".$stroka2." ".$stroka3;
+     return $my_fullname;
+ }
+
+ //Функция разбития строки на три элемента для массива surname,name,patronomyc
+ function getPartsFromFullname($fullNameFunction){
+
+         $pieces = explode(" ", $fullNameFunction);
+ //        $personality[$i]['surname'] = $pieces[0];
+ //        $personality[$i]['name'] = $pieces[1];
+ //        $personality[$i]['patronomyc'] = $pieces[2];
+
+     echo '<pre>';
+     echo var_export($pieces);
+     echo '</pre>';
+     return $pieces;
+ }
+
+ //функция сокращения имени
+ function getShortName ($fullNameFunction){
+
+     $pieces = explode(" ", $fullNameFunction);
+
+//   $pieces[0];
+     $pieces[1] = mb_substr(( $pieces[1]),0,1,"UTF-8");
+     $shortName = $pieces[0]." ". $pieces[1];
+
+     echo $shortName."</br>";
+     return $shortName;
+
+ }
+
+ //функция определения пола
+ function getGenderFromName($fullNameFunction){
+
+      $floor = 0;
+      $pieces = explode(" ", $fullNameFunction);
+
+//      $pieces[$key][0] = $personality[$key]['surname'];
+//      $pieces[$key][1] = $personality[$key]['name'];
+//      $pieces[$key][2] = $personality[$key]['patronomyc'];
+
+      //проверка на фамилии
+        if ( (substr( ( $pieces[0]), -4) ) == "ва" ){
+            $floor--;
+        }elseif( ( (substr( ( $pieces[0]), -2) ) == "в" ) ){
+            $floor++;
+        }else{}
+
+      //проверка на имени
+        if ( (substr( ( $pieces[1]), -2) ) == "а" ){
+            $floor--;
+        }elseif(  ( (substr( ( $pieces[1]), -2) ) == "й" ) ||  ( (substr( ( $pieces[1]), -2) ) == "н" )  ){
+            $floor++;
+        }else{}
+
+
+        //проверка на отчество
+        if ((substr( ( $pieces[2]), -6) ) == "вна" ){
+            $floor--;
+        }elseif(  ((substr( ( $pieces[2]), -4) ) == "ич" )  ){
+            $floor++;
+        }else{}
+
+
+        //вычисление пола
+        switch ($floor){
+            case ( $floor >= 0 ):
+                //муж
+                $floor = 1;
+                break;
+            case ( $floor <= 0 ):
+                //жен
+                $floor = -1;
+                break;
+            default :
+                $floor = 0;
+         }
+
+     return $floor;
+ }
+
+ //функция статистики
+ function getGenderDescription($array_floor)
+ {
+     $array_floor;
+     //Фильтры для статистики
+     //мужицкий фильтр
+     function filterArrayMan($value)
+     {
+         return ($value == 1);
+     }
+
+     $filteredArrayMan = array_filter($array_floor, 'filterArrayMan');
+     //женский
+     function filterArrayGerl($value)
+     {
+         return ($value == -1);
+     }
+
+     $filteredArrayGerl = array_filter($array_floor, 'filterArrayGerl');
+
+     //третий фильтр
+     function filterArrayOno($value)
+     {
+         return ($value == 0);
+     }
+
+     $filteredArrayOno = array_filter($array_floor, 'filterArrayOno');
+
+     //сама статистика
+     $all_man = round(((100 / (count($array_floor))) * count($filteredArrayMan)), 1);
+     $all_gerl = round(((100 / (count($array_floor))) * count($filteredArrayGerl)), 1);
+     $all_ono = round(((100 / (count($array_floor))) * count($filteredArrayOno)), 1);
+
+
+     $statistic = [$all_man , $all_gerl , $all_ono];
+
+     echo "Гендерный состав аудитории:<br>";
+     echo "---------------------------<br>";
+     echo "Мужчины - $all_man%<br>";
+     echo "Женщины  - $all_gerl%<br>";
+     echo "Не удалось определить - $all_ono%<br>";
+ }
+
+
+ //функция идеальной пары
+ function getPerfectPartner($shortName, $array_floor)
+ {
+
+     $shortName;
+     $array_floor;
+
+     $randon_number1 = (rand(0, (count($shortName) - 1)));
+     $randon_number2 = (rand(0, (count($shortName) - 1)));
+
+     //подбор идеальной пары через цикл
+     while (($array_floor[$randon_number1] == $array_floor[$randon_number2]) || ($array_floor[$randon_number1] == 0) || ($array_floor[$randon_number2] == 0)) {
+         $randon_number1 = (rand(0, (count($shortName) - 1)));
+         $randon_number2 = (rand(0, (count($shortName) - 1)));
+     }
+
+     $chose_people1 = $shortName[$randon_number1];
+     $chose_people2 = $shortName[$randon_number2];
+
+
+     $number1 = 5000;
+     $number2 = 10000;
+     $prochent = (rand($number1, $number2)) / 100;
+
+     echo "$chose_people1 + $chose_people2 = <br>";
+     echo "Идеально на $prochent%";
+ }
+
+
+ //разбиваю изначальный масив на части (начало программы)
      foreach ($example_persons_array as $key => $array) {
+
          //присвоил в отдельный масив полные имена
          $myString[$key] = $array['fullname'];
          // разбил на куски
          $pieces = explode(" ", $myString[$key]);
-         $person[$key]['surname'] = $pieces[0];
-         $person[$key]['name'] = $pieces[1];
-         $person[$key]['patronomyc'] = $pieces[2];
+
+         //вызов функции склеивания
+         $fullNameFunction = getFullnameFromParts($pieces[0], $pieces[1], $pieces[2]);
+         echo $fullNameFunction."<br>";
+
+         //вызов функции разбиения
+         $partsFromFullname  = getPartsFromFullname ($fullNameFunction);
+
+         //вызов функции сокращенного имени
+         $shortName = getShortName($fullNameFunction);
+
+         //вызов функции определения пола
+         $genderFromName = getGenderFromName($fullNameFunction);
+         echo "пол данного субъекта: $genderFromName <br><br>";
+
+         //масив полов для статы
+         $array_floor[$key] = $genderFromName;
+
+
      }
-     return $person;
 
- }
- person ();
+        //вызов функции определения пола
+        getGenderDescription($array_floor);
 
 
+
+/*
  //собираю строки в месте
  function getFullnameFromParts ($dataSurnameNamePatronomyc){
      $person = $dataSurnameNamePatronomyc;
@@ -94,6 +267,8 @@
 
 
  }
+
+ $fullNameFunction = getFullnameFromParts(person());
 
 
  //разбиваю строку на три строки surname,name,patronomyc(пункт из задания)
@@ -113,7 +288,7 @@
 
  }
 
-
+/*
 
  //функция сокращения имени
  function getShortName ($personality){
@@ -192,7 +367,6 @@
   }
 
 
-
    //функция статистики
    function getGenderDescription($array_floor)
    {
@@ -263,32 +437,7 @@ function getPerfectPartner($shortName, $array_floor){
 
 }
 
-
-?>
-<!--//////////////////////////////////////////////////////////////////////////////-->
-<h2>Массив с собранными именим и т.д</h2>
- <?php
- $fullNameFunction = getFullnameFromParts(person());
- ?>
- <h2>Массив с разделенными именами и т.д</h2>
-  <?php
- $nameAndFunction = getPartsFromFullname ($fullNameFunction);
-  ?>
- <h2>Массив с сокращенными фамилиями и инициалами</h2>
- <?php
- $shortName = getShortName($nameAndFunction);
- ?>
- <h2>Функция определения пола</h2>
- <?php
- $manOrGerl = getGenderFromName($nameAndFunction);
- ?>
- <h2>Статистика</h2>
- <?php
- getGenderDescription($manOrGerl);
- ?>
- <h2>Совместимость</h2>
- <?php
- getPerfectPartner($shortName, $manOrGerl);
+*/
  ?>
  <br>
  <br>
